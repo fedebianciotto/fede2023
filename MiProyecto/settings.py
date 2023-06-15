ed9b22fd6d88e56dd7faa38d2bc4c537f45ebf62
Federico Bianciotto <federicobianciotto@lopezhnos.com.ar>
2023-06-14 21:44:22 -0300

    sexto

--- MiProyecto/settings.py
+++ MiProyecto/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'MiProyecto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'HOST': "db.tvximfcqlfeoeibficps.supabase.co",
         'NAME': "postgres",
         'USER': "postgres ",
