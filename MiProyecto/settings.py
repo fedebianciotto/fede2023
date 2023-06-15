c50fc0e7325e6d6c5e7b814d2d1d7af80a1863fc
Federico Bianciotto <federicobianciotto@lopezhnos.com.ar>
2023-06-14 21:37:53 -0300

    quinto

--- MiProyecto/settings.py
+++ MiProyecto/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'MiProyecto.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-   # 'default': {
-   #     'ENGINE': 'django.db.backends.sqlite3',
-   #     'NAME': BASE_DIR / 'db.sqlite3',
-   # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': "db.tvximfcqlfeoeibficps.supabase.co",
+        'NAME': "postgres",
+        'USER': "postgres ",
+        'PASSWORD': "n03D2vTsErmFAJBi",
+        'PORT': "5432",
+
+    }
 }
 
 
